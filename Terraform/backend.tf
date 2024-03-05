f54f57ed38a322934da7befea9839811b4e01b27
Mukund Pandey <mukund.pandey@gmail.com>
2024-03-05 21:12:34 +0000

    configuring tfstate

---
+++ Terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "DataPipelineRG"
+    storage_account_name  = "datapipelinestoragejuzj"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
