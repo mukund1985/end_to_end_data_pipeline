3e4fc512c2f2310b6d52a3de5ffccdf563c0a7b6
Mukund Pandey <mukund.pandey@gmail.com>
2024-03-06 03:52:09 +0000

    updating initial_setup.yml

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,18 +1,18 @@
-variable "arm_subscription_id" {
-  description = "The Subscription ID used for the Azure provider"
-}
+# variable "arm_subscription_id" {
+#   description = "The Subscription ID used for the Azure provider"
+# }
 
-variable "arm_tenant_id" {
-  description = "The Tenant ID used for the Azure provider"
-}
+# variable "arm_tenant_id" {
+#   description = "The Tenant ID used for the Azure provider"
+# }
 
-variable "arm_client_id" {
-  description = "The Client ID used for the Azure provider"
-}
+# variable "arm_client_id" {
+#   description = "The Client ID used for the Azure provider"
+# }
 
-variable "arm_client_secret" {
-  description = "The Client Secret used for the Azure provider"
-}
+# variable "arm_client_secret" {
+#   description = "The Client Secret used for the Azure provider"
+# }
 
 variable "resource_group_name" {
   description = "The name of the Resource Group"
