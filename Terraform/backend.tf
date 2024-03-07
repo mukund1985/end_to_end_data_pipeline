d91786ee4378214a4d6b3dd4232ca03106ec1ccd
mukund1985 <mukund.pandey@gmail.com>
2024-03-07 21:40:39 +0000

    Update backend.tf with Azure storage account details

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "datapipelinestoragenvbi"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    access_key           = "4Rl8/P6XnDhdODdDmgVXcjnFJRP5Pi/U7HkZeisHBkxqlO1VwxDykpcKf52KR57o5r9OAyeuNXv9+AStXH0MHw=="
+    access_key           = ""
   }
 }
