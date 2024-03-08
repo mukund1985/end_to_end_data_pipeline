a386d4dd7096799a1193f886d50da9891bb271f0
Mukund Pandey <mukund.pandey@gmail.com>
2024-03-08 11:17:54 +0000

    updating the yml

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,23 +1,15 @@
-# variable "arm_subscription_id" {
-#   description = "The Subscription ID used for the Azure provider"
-# }
-
-# variable "arm_tenant_id" {
-#   description = "The Tenant ID used for the Azure provider"
-# }
-
-# variable "arm_client_id" {
-#   description = "The Client ID used for the Azure provider"
-# }
-
-# variable "arm_client_secret" {
-#   description = "The Client Secret used for the Azure provider"
-# }
-
 variable "resource_group_name" {
   description = "The name of the Resource Group"
 }
 
 variable "location" {
   description = "The location of the Azure resources"
+}
+
+variable "storage_account_name" {
+  description = "The name of the storage account"
+}
+
+variable "container_name" {
+  description = "The name of the container within the storage account"
 }
