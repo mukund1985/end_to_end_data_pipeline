ef0ef0d8eb7619605c3fad6d08b4bdc4674bd5f2
Mukund Pandey <mukund.pandey@gmail.com>
2024-03-09 00:22:50 +0000

    updating script

--- Terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "DataPipelineRG"
-    storage_account_name = "datapipelinestoragezbkj"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    access_key           = ""
-  }
-}
