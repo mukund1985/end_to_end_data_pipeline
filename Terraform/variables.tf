23d1b02ab944111540734357d3cf2630d1adee22
Mukund Pandey <mukund.pandey@gmail.com>
2024-03-11 11:27:06 +0000

    adding network module

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,5 +1,3 @@
-# Variable Declarations with Descriptions for Clarity and Future Reference
-
 # Resource Group Name
 variable "resource_group_name" {
   description = "The name of the Resource Group."
@@ -30,49 +28,69 @@ variable "tenant_id" {
   description = "The Azure AD tenant ID."
 }
 
-# SKU Name
+# SKU Name for the Key Vault
 variable "sku_name" {
   description = "The SKU name for the Key Vault. Possible values are standard and premium."
+  type        = string
   default     = "standard"
 }
 
 # Soft Delete Retention Days
 variable "soft_delete_retention_days" {
   description = "Number of days to retain deleted items before permanent deletion."
+  type        = number
+  default     = 90
 }
 
 # Purge Protection Enabled
 variable "purge_protection_enabled" {
   description = "Enable or disable purge protection for the Key Vault."
+  type        = bool
+  default     = true
 }
 
 # Network ACLs Default Action
 variable "network_acls_default_action" {
   description = "Default action for network ACLs."
+  type        = string
+  default     = "Deny"
 }
 
 # Network ACLs Bypass
 variable "network_acls_bypass" {
   description = "Specifies which traffic can bypass the network ACLs."
+  type        = string
+  default     = "AzureServices"
 }
 
-# For Network module 
+# Deployment Environment Prefix
 variable "environment" {
   description = "The deployment environment prefix for resource naming."
+  type        = string
 }
 
+# Virtual Network Name
 variable "vnet_name" {
   description = "The name of the virtual network."
+  type        = string
 }
 
+# Virtual Network Address Space
 variable "vnet_address_space" {
   description = "The address space for the virtual network in CIDR notation."
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
 }
 
+# Subnet Name
 variable "subnet_name" {
   description = "The name of the subnet."
+  type        = string
 }
 
+# Subnet Address Prefixes
 variable "subnet_address_prefixes" {
   description = "The address prefixes for the subnet in CIDR notation."
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
 }
