7b504373e362a10f8cb58a0883c748370cd47d94
Mukund Pandey <mukund.pandey@gmail.com>
2024-03-15 07:18:02 +0000

    updating data_factory module

--- Terraform/modules/data_factory/main.tf
+++ Terraform/modules/data_factory/main.tf
@@ -7,11 +7,15 @@ resource "azurerm_data_factory" "data_factory" {
   }
 }
 
-resource "azurerm_data_factory_linked_service_blob" "blob_storage" {
+resource "azurerm_data_factory_linked_service_blob_storage" "blob_storage" {
   name            = "${var.environment}-blobstorage"
   data_factory_id = azurerm_data_factory.data_factory.id
   description     = "Linked Service for Azure Blob Storage"
 
-  connection_string = var.storage_account_connection_string
+  connection_string {
+    value = var.storage_account_connection_string
+    type  = "SecureString"
+  }
 }
+
 
