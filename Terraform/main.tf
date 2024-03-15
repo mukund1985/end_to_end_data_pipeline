4286f9739d4feefa9e453282491408477d6e8558
Mukund Pandey <mukund.pandey@gmail.com>
2024-03-15 06:55:19 +0000

    formatting

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -25,16 +25,16 @@ resource "random_string" "sa_suffix" {
 }
 
 module "security" {
-  source                      = "./modules/security"            
-  key_vault_name              = var.key_vault_name              
-  location                    = var.location                    
-  resource_group_name         = azurerm_resource_group.rg.name  
-  tenant_id                   = var.tenant_id                   
-  sku_name                    = var.sku_name                    
-  soft_delete_retention_days  = var.soft_delete_retention_days  
-  purge_protection_enabled    = var.purge_protection_enabled    
-  network_acls_default_action = var.network_acls_default_action 
-  network_acls_bypass         = var.network_acls_bypass         
+  source                      = "./modules/security"
+  key_vault_name              = var.key_vault_name
+  location                    = var.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = var.tenant_id
+  sku_name                    = var.sku_name
+  soft_delete_retention_days  = var.soft_delete_retention_days
+  purge_protection_enabled    = var.purge_protection_enabled
+  network_acls_default_action = var.network_acls_default_action
+  network_acls_bypass         = var.network_acls_bypass
 }
 
 module "network" {
@@ -49,11 +49,11 @@ module "network" {
 }
 
 module "data_factory" {
-  source                              = "./modules/data_factory"
-  environment                         = var.environment
-  location                            = var.location
-  resource_group_name                 = azurerm_resource_group.rg.name
-  storage_account_connection_string   = var.storage_account_connection_string
+  source                            = "./modules/data_factory"
+  environment                       = var.environment
+  location                          = var.location
+  resource_group_name               = azurerm_resource_group.rg.name
+  storage_account_connection_string = var.storage_account_connection_string
 }
 
 
@@ -65,13 +65,13 @@ module "data_lake" {
 }
 
 module "synapse" {
-  source                      = "./modules/synapse"
-  environment                 = var.environment
-  location                    = var.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  sql_admin_password          = var.sql_admin_password
+  source                       = "./modules/synapse"
+  environment                  = var.environment
+  location                     = var.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  sql_admin_password           = var.sql_admin_password
   data_lake_gen2_filesystem_id = module.data_lake.data_lake_gen2_filesystem_id
-  sql_admin_login             = var.sql_admin_login
+  sql_admin_login              = var.sql_admin_login
 }
 
 
